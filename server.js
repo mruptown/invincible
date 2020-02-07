c6de95f462017bd728d01a8813d3a055e0e949be
Joel Wiegman <joel_wiegman@anfcorp.com>
2020-02-07 14:53:31 -0500

    Initial database connectivity

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
+require('dotenv').config();
+require('./server/db-conn');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname });
+});
 
 const {PORT} = process.env;
 app.listen(PORT, () => console.log(`Wizardry happening on port ${PORT}`))
