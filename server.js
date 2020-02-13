28536bd116495b209a8b01ab94292dbc103e3140
Joel Wiegman <joel_wiegman@anfcorp.com>
2020-02-13 11:29:32 -0500

    Cleaning up server.js

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.get('/', (req, res) => {
 app.get('/api/profit/add', (req, res) => {
     var incrementedValue = Math.floor((Math.random() * 5) + 1);
     console.log('Adding ' + incrementedValue + ' to profit!');
-    //Profit.updateOne({}, {$inc: amount + incrementedValue });
-    Profit.findOne({}, (err, profit) => {
-        res.json(profit);
+
+    //this needs to be async
+    let profit = /* await */ Profit.findOneAndUpdate({}, {$inc: { 'amount' : incrementedValue }}, {
+        new: true
     });
+    res.send('Profit = ' + profit.amount);
 });
 
 const {PORT} = process.env;
