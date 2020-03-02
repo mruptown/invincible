3dd36a0242966eb87fc466d8b79165ad3648e1c7
luketeaford <luketeaford@gmail.com>
2020-03-02 13:19:10 -0500

    Reset animal

--- routes/profit.js
+++ routes/profit.js
@@ -12,7 +12,7 @@ router.get('/', async function (req, res, next) {
   } = everything
 
   const SERVER_LOCATION = getServerLocation(req.headers.host)
-  const FAVORITE_ANIMAL = 'Dragons'
+  const FAVORITE_ANIMAL = ''
 
   res.render('profit', {
     title: 'Profits',
