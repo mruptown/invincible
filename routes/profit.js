a7b31b188576a061ba4bb1cf92a8a36a7f1cd019
luketeaford <luketeaford@gmail.com>
2020-07-09 09:37:38 -0400

    Show mottos

--- routes/profit.js
+++ routes/profit.js
@@ -12,7 +12,7 @@ router.get('/', async function (req, res, next) {
   } = everything
 
   const SERVER_LOCATION = getServerLocation(req.headers.host)
-  const SHOW_MOTTOS = false
+  const SHOW_MOTTOS = true
 
   res.render('profit', {
     title: 'Profits',
